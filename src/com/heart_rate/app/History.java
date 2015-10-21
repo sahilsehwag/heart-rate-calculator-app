20f20498e7e8646266dcf042287bf5cfa368a40a
jaat <sehwagsahil002@gmail.com>
2015-10-21 23:54:30 +0530

    Required Tab Files added

---
+++ src/com/heart_rate/app/History.java
@@ -0,0 +1,15 @@
+package com.heart_rate.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jaat on 21-10-2015.
+ */
+public class History extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history);
+    }
+}
