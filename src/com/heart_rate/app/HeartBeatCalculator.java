d7bc8354b458fd443c065d17ab85c629e4e37c05
jaat <sehwagsahil002@gmail.com>
2015-10-24 16:29:32 +0530

    Features added  :    1. User can manually start calculator.    2. Added timer.    3. Some minor changes to UI.
     Bugs Remaining  :
        1. On restart(minimize) of app surface texture is not available.
        2. Sometimes inaccurate reading.

--- src/com/heart_rate/app/HeartBeatCalculator.java
+++ src/com/heart_rate/app/HeartBeatCalculator.java
@@ -2,19 +2,23 @@ package com.heart_rate.app;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.*;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Timer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -26,12 +30,18 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
     private static final String TAG = "HeartRateMonitor";
     private static final AtomicBoolean processing = new AtomicBoolean(false);
 
+    //SURFACE VIEW
     private static SurfaceView preview = null;
     private static SurfaceHolder previewHolder = null;
+
+    //TEXTURE VIEW
     private static TextureView previewSurface = null;
+
     private static Camera camera = null;
     private static View image = null;
     private static TextView text = null;
+    private static TextView timer = null;
+    private TextView beatsCounter = null;
 
     private static PowerManager.WakeLock wakeLock = null;
 
@@ -39,34 +49,6 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
     private static final int averageArraySize = 4;
     private static final int[] averageArray = new int[averageArraySize];
 
-
-
-    @Override
-    public void onClick(View v) {
-//        Toast toast = Toast.makeText(HeartBeatCalculator.this, "touch", Toast.LENGTH_SHORT);
-//        toast.show();
-//        if(v.getId() == R.id.image){
-//            //SETTING PREVIEW DISPLAY(WILL SHOW PREVIEW ON SURFACE VIEW)
-//            try {
-//                //camera.setPreviewDisplay(previewHolder);
-//                camera.setPreviewTexture(previewSurface.getSurfaceTexture());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            //SETTING CAMERA PARAMETERS(SETTING FLASH_MODE AS TORCH)
-//            if(camera != null){
-//                Camera.Parameters parameters = camera.getParameters();
-//                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-//                camera.setParameters(parameters);
-//            }
-//
-//            //SETTING CAMERA PREVIEW CALLBACK AND STARTING PREVIEW
-//            camera.setPreviewCallback(previewCallback);
-//            camera.startPreview();
-//            startTime = System.currentTimeMillis();
-//        }
-    }
 
     public static enum TYPE {
         BLACK, COLORED
@@ -80,7 +62,9 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
     private static final int[] beatsArray = new int[beatsArraySize];
     private static double beats = 0;
     private static long startTime = 0;
-
+    private static long timerStart = 0;
+    private static long timerEnd = 0;
+    private static long elapsedTime = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,46 +81,30 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
         previewSurface = (TextureView) findViewById(R.id.previewSurface);
         previewSurface.setSurfaceTextureListener(this);
 
-
         image = findViewById(R.id.image);
         text = (TextView) findViewById(R.id.beats_count);
+        timer = (TextView) findViewById(R.id.timer);
+        beatsCounter = (TextView) findViewById(R.id.beats_count);
 
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "DoNotDimScreen");
+
+        //ONCLICKLISTENER SETUP
+        image.setOnClickListener(this);
     }
-
-
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
+
     @Override
     public void onResume() {
         super.onResume();
-
         text.setText("");
+        createToast("Tap To Start");
+
         //ACQUIRING WAKE LOCK AND OPENING THE CAMERA
         wakeLock.acquire();
         camera = Camera.open();
-
-        //SETTING PREVIEW DISPLAY(WILL SHOW PREVIEW ON SURFACE VIEW)
-        try {
-            //camera.setPreviewDisplay(previewHolder);
-            camera.setPreviewTexture(previewSurface.getSurfaceTexture());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //SETTING CAMERA PARAMETERS(SETTING FLASH_MODE AS TORCH)
-        if(camera != null){
-            Camera.Parameters parameters = camera.getParameters();
-            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(parameters);
-        }
-
-        //SETTING CAMERA PREVIEW CALLBACK AND STARTING PREVIEW
-        camera.setPreviewCallback(previewCallback);
-        camera.startPreview();
-        startTime = System.currentTimeMillis();
     }
     @Override
     public void onPause() {
@@ -144,97 +112,139 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
         wakeLock.release();
 
         try {
-            camera.setPreviewCallback(null);
-            camera.stopPreview();
+            stopHeartRateCalculator();
             camera.release();
             camera = null;
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.image){
+            if(timerStart == 0 && timerEnd == 0) {
+                beatsCounter.setText("");
+                timerStart = System.currentTimeMillis() / 1000;
+                timerEnd = timerStart;
 
-
-
-    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
-        @Override
-        public void onPreviewFrame(byte[] data, Camera cam) {
-            if (data == null) throw new NullPointerException();
-            Camera.Size size = cam.getParameters().getPreviewSize();
-            if (size == null) throw new NullPointerException();
-
-            if (!processing.compareAndSet(false, true)) return;
-
-            int width = size.width;
-            int height = size.height;
-
-            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
-            if (imgAvg == 0 || imgAvg == 255) {
-                processing.set(false);
-                return;
-            }
-
-            int averageArrayAvg = 0;
-            int averageArrayCnt = 0;
-            for (int i = 0; i < averageArray.length; i++) {
-                if (averageArray[i] > 0) {
-                    averageArrayAvg += averageArray[i];
-                    averageArrayCnt++;
+                //SETTING PREVIEW DISPLAY(WILL SHOW PREVIEW ON SURFACE VIEW)
+                try {
+                    //camera.setPreviewDisplay(previewHolder);
+                    camera.setPreviewTexture(previewSurface.getSurfaceTexture());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }
 
-            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
-            TYPE newType = currentType;
-            if (imgAvg < rollingAverage) {
-                newType = TYPE.COLORED;
-                if (newType != currentType) {
-                    beats++;
+                //SETTING CAMERA PARAMETERS(SETTING FLASH_MODE AS TORCH)
+                if (camera != null) {
+                    Camera.Parameters parameters = camera.getParameters();
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(parameters);
                 }
-            } else if (imgAvg > rollingAverage) {
-                newType = TYPE.BLACK;
+
+                //SETTING CAMERA PREVIEW CALLBACK AND STARTING PREVIEW
+                camera.setPreviewCallback(previewCallback);
+                camera.startPreview();
+                startTime = System.currentTimeMillis();
             }
+        }
+    }
 
-            if (averageIndex == averageArraySize) averageIndex = 0;
-            averageArray[averageIndex] = imgAvg;
-            averageIndex++;
 
-            // Transitioned from one state to another to the same
-            if (newType != currentType) {
-                currentType = newType;
-                image.postInvalidate();
-            }
+    private static Camera.PreviewCallback previewCallback;
+    static {
+        previewCallback = new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] data, Camera cam) {
+                if (data == null) throw new NullPointerException();
+                Camera.Size size = cam.getParameters().getPreviewSize();
+                if (size == null) throw new NullPointerException();
 
-            long endTime = System.currentTimeMillis();
-            double totalTimeInSecs = (endTime - startTime) / 1000d;
-            if (totalTimeInSecs >= 10) {
-                double bps = (beats / totalTimeInSecs);
-                int dpm = (int) (bps * 60d);
-                if (dpm < 30 || dpm > 180) {
-                    startTime = System.currentTimeMillis();
-                    beats = 0;
+                //HANDLING TIMER
+                if(elapsedTime <= 0) {
+                    stopHeartRateCalculator();
+                    return;
+                }
+                timerEnd = System.currentTimeMillis() / 1000;
+                elapsedTime = 30 - (timerEnd - timerStart);
+                timer.setText("Measuring in" + "\n         " + String.valueOf(elapsedTime) + "s");
+
+
+                if (!processing.compareAndSet(false, true)) return;
+
+                int width = size.width;
+                int height = size.height;
+
+                int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
+                if (imgAvg == 0 || imgAvg == 255) {
                     processing.set(false);
                     return;
                 }
 
-                if (beatsIndex == beatsArraySize) beatsIndex = 0;
-                beatsArray[beatsIndex] = dpm;
-                beatsIndex++;
-
-                int beatsArrayAvg = 0;
-                int beatsArrayCnt = 0;
-                for (int i = 0; i < beatsArray.length; i++) {
-                    if (beatsArray[i] > 0) {
-                        beatsArrayAvg += beatsArray[i];
-                        beatsArrayCnt++;
+                int averageArrayAvg = 0;
+                int averageArrayCnt = 0;
+                for (int i = 0; i < averageArray.length; i++) {
+                    if (averageArray[i] > 0) {
+                        averageArrayAvg += averageArray[i];
+                        averageArrayCnt++;
                     }
                 }
-                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
-                text.setText(String.valueOf(beatsAvg));
-                startTime = System.currentTimeMillis();
-                beats = 0;
+
+                int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+                TYPE newType = currentType;
+                if (imgAvg < rollingAverage) {
+                    newType = TYPE.COLORED;
+                    if (newType != currentType) {
+                        beats++;
+                    }
+                } else if (imgAvg > rollingAverage) {
+                    newType = TYPE.BLACK;
+                }
+
+                if (averageIndex == averageArraySize) averageIndex = 0;
+                averageArray[averageIndex] = imgAvg;
+                averageIndex++;
+
+                // Transitioned from one state to another to the same
+                if (newType != currentType) {
+                    currentType = newType;
+                    image.postInvalidate();
+                }
+
+                long endTime = System.currentTimeMillis();
+                double totalTimeInSecs = (endTime - startTime) / 1000d;
+                if (totalTimeInSecs >= 10) {
+                    double bps = (beats / totalTimeInSecs);
+                    int dpm = (int) (bps * 60d);
+                    if (dpm < 30 || dpm > 180) {
+                        startTime = System.currentTimeMillis();
+                        beats = 0;
+                        processing.set(false);
+                        return;
+                    }
+
+                    if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                    beatsArray[beatsIndex] = dpm;
+                    beatsIndex++;
+
+                    int beatsArrayAvg = 0;
+                    int beatsArrayCnt = 0;
+                    for (int i = 0; i < beatsArray.length; i++) {
+                        if (beatsArray[i] > 0) {
+                            beatsArrayAvg += beatsArray[i];
+                            beatsArrayCnt++;
+                        }
+                    }
+                    int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                    text.setText(String.valueOf(beatsAvg));
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                }
+                processing.set(false);
             }
-            processing.set(false);
-        }
-    };
+        };
+    }
+
 
 
     private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
@@ -266,7 +276,6 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
             }
         }
     };
-
     private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
         Camera.Size result = null;
 
@@ -291,14 +300,7 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
     //SURFACE TEXTURE VIEW LISTENER METHODS
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-        try {
-            camera.setPreviewTexture(surface);
-        } catch (Throwable t) {
-            Log.e("PreviewDemo-surfaceCallback", "Exception in setPreviewDisplay()", t);
-        }
-
         Camera.Parameters parameters = camera.getParameters();
-        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         Camera.Size size = getSmallestPreviewSize(width, height, parameters);
 
 
@@ -307,13 +309,6 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
             Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
         }
         camera.setParameters(parameters);
-
-        try{
-            camera.setPreviewTexture(surface);
-            camera.setPreviewCallback(previewCallback);
-        } catch (IOException t) {}
-
-        camera.startPreview();
         previewSurface.setVisibility(View.INVISIBLE); // Make the surface invisible as soon as it is created
     }
     @Override
@@ -327,5 +322,30 @@ public class HeartBeatCalculator extends Activity implements TextureView.Surface
     @Override
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
         // Update your view here!
+    }
+
+
+    //PRIVATE METHODS
+    private void createToast(CharSequence message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    private static void stopHeartRateCalculator(){
+        try {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setFlashMode(parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            timerEnd = 0;
+            timerStart = 0;
+            elapsedTime = 30;
+            beats = 0;
+            timer.setText("");
+            for(int value : beatsArray) value = 0;
+            for(int value : averageArray) value = 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/heart_rate/app/HeartbeatReference.java
+++ src/com/heart_rate/app/HeartbeatReference.java
@@ -2,7 +2,6 @@ package com.heart_rate.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 /**
  * Created by jaat on 18-10-2015.
@@ -12,6 +11,6 @@ public class HeartbeatReference extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.heart_beat_ref);
+        setContentView(R.layout.heart_beat_reference);
     }
 }
